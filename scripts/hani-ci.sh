1bbcb55e110dcc8b40b58c48be510a8ecaf9c3d9
masseko68 <masseko68@gmail.com>
2025-06-18 17:58:05 +0000

    updateeeeeeeeeeee

--- scripts/hani-ci.sh
+++ scripts/hani-ci.sh
@@ -72,30 +72,28 @@ if [[ $1 = "-b" || $1 = "--build" ]]; then
     echo $PATH
     echo -e "\n📂 Setting up defconfig..."
     make O=out ARCH=arm64 \
-        CC="clang" \
+        CC=clang \
         CROSS_COMPILE=aarch64-linux-gnu- \
-        AR="llvm-ar" \
-	    NM="llvm-nm" \
-	    LD="ld.lld -S" \
-	    OBJCOPY="llvm-objcopy" \
-	    OBJDUMP="llvm-objdump" \
-        LLVM=1 \
-        LLVM_IAS=1 \
+        LD=ld.lld \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
         STRIP=llvm-strip \
+        LLVM=1 LLVM_IAS=1 \
         $DEFCONFIG
 
     echo -e "\n🚀 Starting kernel build..."
     make -j$(nproc --all) O=out ARCH=arm64 \
-        CC="clang" \
+        CC=clang \
         CROSS_COMPILE=aarch64-linux-gnu- \
-        AR="llvm-ar" \
-	    NM="llvm-nm" \
-	    LD="ld.lld -S" \
-	    OBJCOPY="llvm-objcopy" \
-	    OBJDUMP="llvm-objdump" \
-        LLVM=1 \
-        LLVM_IAS=1 \
-        STRIP=llvm-strip || exit 1
+        LD=ld.lld \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        LLVM=1 LLVM_IAS=1 \ || exit 1
 
     echo -e "\n✅ Build completed in $SECONDS seconds."
     exit 0
