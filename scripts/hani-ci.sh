f9e094f195ef9cd1c6d957273cc983bf5ed9a16c
masseko68 <masseko68@gmail.com>
2025-06-18 17:45:29 +0000

    testing lagi

--- scripts/hani-ci.sh
+++ scripts/hani-ci.sh
@@ -18,19 +18,20 @@ export KBUILD_BUILD_VERSION=69
 export KBUILD_BUILD_USER=hani
 export KBUILD_BUILD_HOST=dungeon
 export PATH="$CLANG_PATH/bin:$GCC64_PATH/bin:$GCC32_PATH/bin:$PATH"
+echo -e "\n$PATH"
 
 # ========== TOOLCHAIN DOWNLOADER ==========
 if [[ $1 = "-t" || $1 = "--tools" ]]; then
     mkdir -p toolchain 
     cd toolchain
 
-    # -------- CLANG --------
-    # echo "📦 Downloading AOSP Clang (r547379)..."
-    # aria2c -x 16 -s 16 -c -o clang.tar.gz \
-    #   "https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379/-/archive/15.0/android_prebuilts_clang_host_linux-x86_clang-r547379-15.0.tar.gz" || exit 1
-    # mkdir -p clang && tar -xf clang.tar.gz -C clang
-    # rm clang.tar.gz
-    # echo "✅ Clang extracted to: $(pwd)/clang"
+    -------- CLANG --------
+    echo "📦 Downloading AOSP Clang (r547379)..."
+    aria2c -x 16 -s 16 -c -o clang.tar.gz \
+      "https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379/-/archive/15.0/android_prebuilts_clang_host_linux-x86_clang-r547379-15.0.tar.gz" || exit 1
+    mkdir -p clang && tar -xf clang.tar.gz -C clang
+    rm clang.tar.gz
+    echo "✅ Clang extracted to: $(pwd)/clang"
 
     # -------- GCC 64-bit --------
     echo "📦 Downloading AOSP GCC 64-bit..."
@@ -40,23 +41,16 @@ if [[ $1 = "-t" || $1 = "--tools" ]]; then
     rm gcc64.tar.gz
     echo "✅ GCC 64-bit extracted."
 
-    # # -------- GCC 32-bit --------
-    # echo "📦 Downloading AOSP GCC 32-bit..."
-    # aria2c -x 16 -s 16 -c -o gcc32.tar.gz \
-    #   "https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz" || exit 1
-    # mkdir -p GCC-32 && tar -xzf gcc32.tar.gz -C GCC-32
-    # rm gcc32.tar.gz
-    # echo "✅ GCC 32-bit extracted."
-    # echo "--- Contents within clang toolchain ---"
-    # ls "$(pwd)/toolchain/clang"/* echo "" # Add a blank line for better readability
+    # -------- GCC 32-bit --------
+    echo "📦 Downloading AOSP GCC 32-bit..."
+    aria2c -x 16 -s 16 -c -o gcc32.tar.gz \
+      "https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz" || exit 1
+    mkdir -p GCC-32 && tar -xzf gcc32.tar.gz -C GCC-32
+    rm gcc32.tar.gz
+    echo "✅ GCC 32-bit extracted."
+    echo "--- Contents within clang toolchain ---"
+    ls "$(pwd)/toolchain/clang"/* echo "" # Add a blank line for better readability
 
-    echo "--- Recursive list of all files in GCC-64 ---"
-    # find "$(pwd)/toolchain/GCC-64"
-    echo ""
-
-    echo "--- Tree view of GCC-64 ---"
-    find . -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-
-    echo ""
 
     echo -e "\n🎉 All toolchains downloaded successfully"
     exit 0
@@ -75,10 +69,16 @@ fi
 if [[ $1 = "-b" || $1 = "--build" ]]; then
 	PATH=$PWD/toolchain/clang/bin:$PWD/toolchain/GCC-64/bin:$PWD/toolchain/GCC-32/bin:$PATH
     mkdir -p out
-
+    echo $PATH
     echo -e "\n📂 Setting up defconfig..."
     make O=out ARCH=arm64 \
+        CC="clang" \
         CROSS_COMPILE=aarch64-linux-gnu- \
+        AR="llvm-ar" \
+	    NM="llvm-nm" \
+	    LD="ld.lld -S" \
+	    OBJCOPY="llvm-objcopy" \
+	    OBJDUMP="llvm-objdump" \
         LLVM=1 \
         LLVM_IAS=1 \
         STRIP=llvm-strip \
@@ -86,7 +86,13 @@ if [[ $1 = "-b" || $1 = "--build" ]]; then
 
     echo -e "\n🚀 Starting kernel build..."
     make -j$(nproc --all) O=out ARCH=arm64 \
+        CC="clang" \
         CROSS_COMPILE=aarch64-linux-gnu- \
+        AR="llvm-ar" \
+	    NM="llvm-nm" \
+	    LD="ld.lld -S" \
+	    OBJCOPY="llvm-objcopy" \
+	    OBJDUMP="llvm-objdump" \
         LLVM=1 \
         LLVM_IAS=1 \
         STRIP=llvm-strip || exit 1
