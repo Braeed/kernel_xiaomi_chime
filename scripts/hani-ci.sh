915986d1759127a99ad4e948c8188692f885d36d
masseko68 <masseko68@gmail.com>
2025-06-18 23:46:03 +0000

    updateeeeee

--- scripts/hani-ci.sh
+++ scripts/hani-ci.sh
@@ -25,21 +25,21 @@ if [[ $1 = "-t" || $1 = "--tools" ]]; then
     mkdir -p toolchain 
     cd toolchain
 
-    # -------- CLANG --------
-    # echo "📦 Downloading AOSP Clang (r547379)..."
-    # aria2c -x 16 -s 16 -c -o clang.tar.gz \
-    #   "https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379/-/archive/15.0/android_prebuilts_clang_host_linux-x86_clang-r547379-15.0.tar.gz" || exit 1
-    # mkdir -p clang && tar -xf clang.tar.gz -C clang
-    # rm clang.tar.gz
-    # echo "✅ Clang extracted to: $(pwd)/clang"
     -------- CLANG --------
     echo "📦 Downloading AOSP Clang (r547379)..."
     aria2c -x 16 -s 16 -c -o clang.tar.gz \
-      "https://github.com/topnotchfreaks/clang/releases/download/tnfclang-2025.05.30-13.38.52/topnotchfreaks-android-clang.tar.gz"
+      "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz" || exit 1
     mkdir -p clang && tar -xf clang.tar.gz -C clang
     rm clang.tar.gz
     echo "✅ Clang extracted to: $(pwd)/clang"
     
+    # -------- CLANG --------
+    # echo "📦 Downloading AOSP Clang (r547379)..."
+    # aria2c -x 16 -s 16 -c -o clang.tar.gz \
+    #   "https://github.com/topnotchfreaks/clang/releases/download/tnfclang-2025.05.30-13.38.52/topnotchfreaks-android-clang.tar.gz"
+    # mkdir -p clang && tar -xf clang.tar.gz -C clang
+    # rm clang.tar.gz
+    # echo "✅ Clang extracted to: $(pwd)/clang"
     # -------- GCC 64-bit --------
     echo "📦 Downloading AOSP GCC 64-bit..."
     aria2c -x 16 -s 16 -c -o gcc64.tar.gz \
